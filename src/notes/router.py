aefe16fec049092669b5b93ca692989a0b5de129
Ivan <evankirk@vk.com>
2024-12-12 18:59:37 +0500

    Реализовать эндпоинт для изменения записи

--- src/notes/router.py
+++ src/notes/router.py
@@ -1,12 +1,14 @@
 from typing import Annotated
-from datetime import date
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import (
+    
+    APIRouter, 
+    Depends
+)
 
 from .services import NoteService
-from .schemas import NoteCreate, Note
+from .schemas import NoteCreate, NoteBase
 from .dependencies import get_note_service
-from .enums import Consumable
 
 
 router = APIRouter(
@@ -16,20 +18,10 @@ router = APIRouter(
 )
 
 
-def get_pagination_params(
-        
-    offset: int = Query(0, ge=0),
-    limit: int = Query(10, gt=0)
-
-    ):
-
-    return {"offset": offset, "limit": limit}
-
-
 @router.post('/')
 async def add_note(
 
-    note: NoteCreate, 
+    note: NoteCreate,
     service: Annotated[NoteService, Depends(get_note_service)]
     
     ):
@@ -37,7 +29,7 @@ async def add_note(
     await service.add(note)
 
 
-@router.delete('/')
+@router.delete('/{id}')
 async def delete_note(
 
     id: int, 
@@ -48,59 +40,13 @@ async def delete_note(
     await service.delete(id)
 
 
-@router.patch('/{id}/date')
-async def edit_note_date(
-
+@router.put('/{id}')
+async def update_note(
+    
     id: int, 
-    new_date: date, 
+    note: NoteBase, 
     service: Annotated[NoteService, Depends(get_note_service)]
     
     ):
 
-    await service.edit_date(id, new_date)
-
-
-@router.patch('/{id}/consumable')
-async def edit_note_consumable(
-
-    id: int, 
-    new_consumable: Consumable, 
-    service: Annotated[NoteService, Depends(get_note_service)]
-    
-    ):
-
-    await service.edit_consumable(id, new_consumable)
-
-
-@router.patch('/{id}/count')
-async def edit_note_count(
-
-    id: int, 
-    new_count: int, 
-    service: Annotated[NoteService, Depends(get_note_service)]
-
-    ):
-
-    await service.edit_count(id, new_count)
-
-
-@router.get('/', response_model=list[Note])
-async def get_notes(
-
-    service: Annotated[NoteService, Depends(get_note_service)],
-    pagination: Annotated[dict, Depends(get_pagination_params)]
-
-    ):
-    
-    return await service.get(pagination['limit'], pagination['offset'])
-
-
-@router.get('/{year}')
-async def get_notes_by_year(
-    
-    year: int,
-    service: Annotated[NoteService, Depends(get_note_service)]
-    
-    ):
-
-    return await service.get_by_year(year)
+    await service.update_note(id, note)
